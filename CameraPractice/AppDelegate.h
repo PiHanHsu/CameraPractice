984065fdf4957af0f066671db63213c23ca9f9e4
PiHanHsu <pihanhsu@gmail.com>
2014-12-18 15:32:51 +0800

    Initial Commit

---
+++ CameraPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraPractice
+//
+//  Created by PiHan Hsu on 2014/12/18.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraPractice
+//
+//  Created by PiHan Hsu on 2014/12/18.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
